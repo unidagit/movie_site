2994f663503a0abcbe249be79f20b74f5aff29e1
yunheejo <rhcjrdbsgml@gmail.com>
2022-11-06 11:48:51 +0900

    영화 data fetch

---
+++ src/api.ts
@@ -0,0 +1,28 @@
+const API_KEY = "1061e72afd9ee444b378544b0d109c01";
+
+const BASE_PATH = "https://api.themoviedb.org/3/";
+
+export interface IMovie {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+
+export interface IGetMovieResult {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
+
+export function getMovies() {
+  return fetch(
+    `${BASE_PATH}/movie/now_playing?api_key=${API_KEY}&language=ko&page=1®ion=KR`
+  ).then((response) => response.json());
+}
