cbb987a2835a83a13d072b54f37d6c8ba636313f
yunheejo <rhcjrdbsgml@gmail.com>
2022-11-09 22:50:39 +0900

    api fetch 추가

--- src/api.ts
+++ src/api.ts
@@ -21,14 +21,172 @@ export interface IGetMovieResult {
   total_results: number;
 }
 
+export interface IGenres {
+  [key: string]: string;
+}
+
+export interface IGetMovieDetail {
+  genres: IGenres[];
+  id: number;
+  original_title: string;
+  overview: string;
+  release_date: string;
+  runtime: number;
+  tagline: string;
+  title: string;
+  vote_average: number;
+  vote_count: number;
+  backdrop_path: string;
+  poster_path: string;
+}
+
+export interface IGetTvResult {
+  page: number;
+  results: IShow[];
+  total_pages: number;
+  total_results: number;
+}
+
+export interface IShow {
+  original_name: string;
+  genre_ids: number[];
+  name: string;
+  popularity: number;
+  origin_country: string[];
+  vote_count: number;
+  first_air_date: string;
+  backdrop_path: string;
+  original_language: string;
+  id: number;
+  vote_average: number;
+  overview: string;
+  poster_path: string;
+}
+
+export interface IShowDetail {
+  backdrop_path: string;
+  created_by: {
+    id: number;
+    credit_id: string;
+    name: string;
+    gender: number;
+    profile_path: string;
+  }[];
+  episode_run_time: number[];
+  first_air_date: string;
+  genres: {
+    id: number;
+    name: string;
+  }[];
+  homepage: string;
+  id: number;
+  in_production: boolean;
+  languages: string[];
+  last_air_date: string;
+  last_episode_to_air: {
+    air_date: string;
+    episode_number: number;
+    id: number;
+    name: string;
+    overview: string;
+    production_code: string;
+    season_number: number;
+    still_path: string;
+    vote_average: number;
+    vote_count: number;
+  };
+  name: string;
+  next_episode_to_air: null;
+  networks: {
+    name: string;
+    id: number;
+    logo_path: string;
+    origin_country: string;
+  }[];
+  number_of_episodes: number;
+  number_of_seasons: number;
+  origin_country: string[];
+  original_language: string;
+  original_name: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: {
+    id: number;
+    logo_path: string;
+    name: string;
+    origin_country: string;
+  }[];
+  production_countries: {
+    iso_3166_1: string;
+    name: string;
+  }[];
+  seasons: {
+    air_date: string;
+    episode_count: number;
+    id: number;
+    name: string;
+    overview: string;
+    poster_path: string;
+    season_number: number;
+  }[];
+  spoken_languages: {
+    english_name: string;
+    iso_639_1: string;
+    name: string;
+  }[];
+  status: string;
+  tagline: string;
+  type: string;
+  vote_average: number;
+  vote_count: number;
+}
+
 export function getMovies() {
   return fetch(
-    `${BASE_PATH}/movie/now_playing?api_key=${API_KEY}&language=${LANGUAGE}&region=kr`
+    `${BASE_PATH}/movie/now_playing?api_key=${API_KEY}&language=ko-KR&page=1`
+  ).then((response) => response.json());
+}
+
+export function getDetailsMovies(movieId: number) {
+  return fetch(
+    `${BASE_PATH}/movie/${movieId}?api_key=${API_KEY}&language=ko-KR`
   ).then((response) => response.json());
 }
 
 export function getPopularMovies() {
   return fetch(
     `${BASE_PATH}/movie/popular?api_key=${API_KEY}&language=${LANGUAGE}&&region=kr`
+  ).then((response) => response.json());
+}
+
+export function getTopRated() {
+  return fetch(
+    `${BASE_PATH}/movie/top_rated?api_key=${API_KEY}&language=${LANGUAGE}&&region=kr`
+  ).then((response) => response.json());
+}
+
+//Tv
+export function getAiringTodayTv() {
+  return fetch(
+    `${BASE_PATH}/tv/airing_today?api_key=${API_KEY}&language=ko-KR&page=1`
+  ).then((response) => response.json());
+}
+
+export function getShowDetail(showId: number) {
+  return fetch(
+    `${BASE_PATH}/tv/${showId}?api_key=${API_KEY}&language=${LANGUAGE}`
+  ).then((response) => response.json());
+}
+
+export function getTopRatedShows() {
+  return fetch(
+    `${BASE_PATH}/tv/top_rated?api_key=${API_KEY}&language=${LANGUAGE}&page=1&region=kr`
+  ).then((response) => response.json());
+}
+
+export function getPopularShows() {
+  return fetch(
+    `${BASE_PATH}/tv/popular?api_key=${API_KEY}&language=${LANGUAGE}&page=1&region=kr`
   ).then((response) => response.json());
 }
