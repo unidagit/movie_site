2eaa2847ec1b950b754c3c84fab11052386f783d
yunheejo <rhcjrdbsgml@gmail.com>
2022-12-27 12:37:46 +0900

    삭제

--- src/Route.ts
+++ src/Route.ts
@@ -3,11 +3,6 @@ import { useNavigate } from "react-router-dom";
 export function PrivateRoute({ children }: any) {
   const token = !!localStorage.getItem("token"); //로컬스토리지에 토큰 유무여부
   const navigate = useNavigate();
-  return token ? children : navigate("/login"); //토큰있으면~~ 토큰 없으면 로그인페이지로
-}
 
-export function PublicRoute({ children }: any) {
-  const token = !!localStorage.getItem("token"); //로컬스토리지에 토큰 유무여부
-  const navigate = useNavigate();
-  return token ? navigate("/home") : children; //토큰있으면 홈으로 토큰 없으면
+  return token ? children : navigate("/login"); //토큰있으면~~ 토큰 없으면 로그인페이지로
 }
